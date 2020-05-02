bc549219b1e7006c4d159c5bf90fd8d9a3479cac
draj1010 <45226595+draj1010@users.noreply.github.com>
2020-05-02 09:35:44 +0530

    temp

--- mysite/products/admin.py
+++ mysite/products/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Category
 from django.contrib.auth.models import Group
 # Register your models here.
 
+def change_rating(modeladmin,request,queryset):
+    queryset.update(rating = 'e')
+
+change_rating.short_description = "Rate Excellent"
+
 class ProductA(admin.ModelAdmin):
     # exclude = ('description', )
-    list_display = ('name','description')
+    list_display = ('name','description','rating','mfg_date','catagory')
+    list_filter = ('mfg_date',)
+    actions = [change_rating]
 
 admin.site.register(Product,ProductA)
-admin.site.unregister(Group)
+
+class CategoryA(admin.ModelAdmin):
+    list_display = ('cat_name','parent','slug')
+
+admin.site.register(Category,CategoryA)
+# admin.site.unregister(Group)
 admin.site.site_header = "Draj"
--- mysite/products/models.py
+++ mysite/products/models.py
@@ -1,10 +1,36 @@
 from django.db import models
 
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self',blank=True, null=True, related_name='child',on_delete=models.DO_NOTHING)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('cat_name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.cat_name
+
+
+Rating = [
+    ('b', 'Bad'),
+    ('a', 'Average'),
+    ('e', 'Excellent')
+]
+
 class Product(models.Model):
     name = models.CharField(max_length = 200)
     description = models.TextField()
+    catagory = models.ForeignKey(Category, on_delete=models.DO_NOTHING,null=True)
+    rating = models.CharField(max_length = 1, choices = Rating)
     mfg_date = models.DateTimeField(auto_now_add = True)
     def __str__(self):
         return self.name
     def show_desc(self):
         return self.description[:50]
+
+            
+
